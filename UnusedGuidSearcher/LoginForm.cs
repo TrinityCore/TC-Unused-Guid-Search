35b5d60a3e3a96520486a1c5b8cda2c70f2c9e36
kaelima <kaelima@live.se>
2012-01-06 13:04:10 +0100

    Move testconnection to loginform to allow reconnecting without closing app

--- UnusedGuidSearcher/LoginForm.cs
+++ UnusedGuidSearcher/LoginForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace UnusedGuidSearcher
 {
@@ -12,6 +13,7 @@ namespace UnusedGuidSearcher
         private string _password;
         private string _database;
         private string _host;
+        private string _connectionString;
 
         public LoginForm()
         {
@@ -32,6 +34,7 @@ namespace UnusedGuidSearcher
             _password = PasswordBox.Text;
             _database = DBBox.Text;
             _host = HostBox.Text;
+            _connectionString = string.Format("SERVER={0};DATABASE={1};UID={2};PASSWORD={3};", _host, _database, _username, _password);
 
             _settings.PutSetting("User", _username);
             _settings.PutSetting("Password", _password);
@@ -41,15 +44,47 @@ namespace UnusedGuidSearcher
 
         private void OkButtonClick(object sender, EventArgs e)
         {
+            if (!TestConnection())
+                return;
+
             SaveSettings();
             Close();
-
             new Thread(StartMainForm).Start();
         }
 
         private void StartMainForm()
         {
-            Application.Run(new MainForm(_username, _password, _database, _host));
+            Application.Run(new MainForm(_connectionString));
+        }
+
+        private bool TestConnection()
+        {
+            MySqlConnection connection = null;
+            var result = true;
+
+            try
+            {
+                connection = new MySqlConnection(_connectionString);
+                connection.Open();
+            }
+            catch (MySqlException ex)
+            {
+                // ReSharper disable LocalizableElement
+                MessageBox.Show(ex.Message, "Could not connect.", MessageBoxButtons.RetryCancel,
+                    MessageBoxIcon.Error, MessageBoxDefaultButton.Button1);
+                // ReSharper restore LocalizableElement
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Close();
+                    connection.Dispose();
+                    result = false;
+                }
+            }
+
+            return result;
         }
     }
 }
